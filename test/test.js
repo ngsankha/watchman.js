af25f7c0f78df7ca406fd7dfe61de2ff7110acfa
Sankha Narayan Guria <sankha93@gmail.com>
2013-05-20 02:06:43 +0530

    Stable version

---
+++ test/test.js
@@ -0,0 +1,118 @@
+var assert = require("assert");
+var fs = require("fs");
+var path = require("path");
+var watchman = require("../watchman");
+
+describe('watchman', function() {
+    fs.writeFileSync('test.txt', 'abc');
+
+    // Test for the change trigger
+    describe('watch()', function() {
+        it('should trigger callback by validating expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["true"], {change: changeTrigger});
+            fs.writeFileSync('test.txt', 'abcd');
+        });
+    });
+
+    // Test for the add trigger
+    describe('watch()', function() {
+        it('should trigger callback by validating expression when the unlink event occurs', function(){
+            function addTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["true"], {add: addTrigger});
+            fs.writeFileSync('test.txt', 'abc');
+        });
+    });
+
+    // Test for the allof expression
+    describe('watch()', function() {
+        it('should trigger callback by validating allof expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["allof", ["type", "f"], ["true"]], {change: changeTrigger});
+            fs.writeFileSync('test.txt', 'abcd');
+        });
+    });
+
+    // Test for the anyof expression
+    describe('watch()', function() {
+        it('should trigger callback by validating anyof expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["anyof", ["type", "f"], ["false"]], {change: changeTrigger});
+            fs.writeFileSync('test.txt', 'abc');
+        });
+    });
+
+    // Test for the not expression
+    describe('watch()', function() {
+        it('should trigger callback by validating not expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["not", ["empty"]], {change: changeTrigger});
+            fs.writeFileSync('test.txt', 'abcd');
+        });
+    });
+
+    // Test for the suffix expression
+    describe('watch()', function() {
+        it('should trigger callback by validating allof expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["suffix", ".txt"], {change: changeTrigger});
+            fs.writeFileSync('test.txt', 'abc');
+        });
+    });
+
+    // Test for the name expression
+    describe('watch()', function() {
+        it('should trigger callback by validating name expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["name", "test.txt"], {change: changeTrigger});
+            fs.writeFileSync('test.txt', 'abcd');
+        });
+    });
+
+    // Test for the iname expression
+    describe('watch()', function() {
+        it('should trigger callback by validating iname expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["iname", "TEST.TXT"], {change: changeTrigger});
+            fs.writeFileSync('test.txt', 'abc');
+        });
+    });
+
+    // Test for the empty expression
+    describe('watch()', function() {
+        it('should trigger callback by validating empty expression when the change event occurs', function(){
+            function changeTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["empty"], {change: changeTrigger});
+            fs.writeFileSync('test.txt', '');
+        });
+    });
+
+    // Test for the unlink trigger
+    describe('watch()', function() {
+        it('should trigger callback by validating expression when the unlink event occurs', function(){
+            function unlinkTrigger(file) {
+                assert.equal('test.txt', path.basename(file));
+            }
+            watchman.watch('test.txt', ["true"], {unlink: unlinkTrigger});
+            fs.unlinkSync('test.txt');
+        });
+    });
+});
--- watchman.js
+++ watchman.js
@@ -1,9 +1,134 @@
+// **Project maintained by [sankha93](https://github.com/sankha93) | View on [Github](https://github.com/sankha93/watchman.js) **
+//
+// A file watching API with a powerful expression parser
+//
+// ##Purpose
+//
+// Watchman.js exists to watch files and raise events when files change (or get deleted/added). It can trigger actions (such as recompiling files) by matching the expressions when the files change.
+//
+// ##Concepts
+//
+// Watchman.js can watch one or more files or directory trees. It matches the files that have changed against its corresponding expression. If the expression validates to be true the callback for that event is called.
+//
+// The expression has to be a valid JavaScript Array. It will have tokens which can be expressions themselves, thus allowing you to next expressions. For example, a valid expression would be:
+//
+// `['true']` or `['anyof', ['empty'], ['not', ['exists']]]`
+//
+// ##Expressions
+//
+// A Watchman.js query expression consists of 1 or more terms. The expression is evaluated against the file and produces a boolean result. If that result is true then the file is considered a match and the corresponding callback is called.
+//
+// An expression term is canonically represented as a JSON array whose zeroth element is the string containing the term name.
+//
+//     ["termname", arg1, arg2, ...]
+//
+// The 'termname` can be any of the following:
+//
+// * `allof`: This evaluates to true if all the grouped expressions also evaluated as true. For example this will trigger callbacks only for files that are empty and exist:
+//
+//       ["allof", ["empty"], ["exists"]]
+// Each array element after the term name is evaluated as an expression of its own.
+//
+//       ["allof", expr1, expr2, ...]
+// Evaluation of the subexpressions stops at the first one that returns false.
+// * `anyof`: This evaluates to true if any of the grouped expression evaluates to true. For example this will trigger callbacks only for files that empty or doesn't exist.
+//
+//       ['anyof', ['empty'], ['not', ['exists']]]
+// Each array element after the term name is evaluated as an expression of its own.
+//
+//       ["allof", expr1, expr2, ...]
+// Evaluation of the subexpressions stops at the first one that returns true.
+// * `not`: This is inverts the result of the subexpression argument.
+//
+//       ["not", "empty"]
+// * `true`: The true expression always evaluates as true.
+//
+//       ["true"]
+// * `false`: The false expression always evaluates as false.
+//
+//       ["false"]
+// * `suffix`: This evaluates to true if the file suffix matches the subexpression. This matches file `foo.php` and `foo.PHP` but not `foophp`.
+//
+//       ["suffix", "php"]
+// * `regex`: This performs a full JavaScript Regex match against the full path of the file on which the event has been triggered. It evaluates to true if it matches, false otherwise. This will trigger callbacks for all files that ends in `t`.
+//
+//       ["regex", "/t$/"]
+// * `iregex`: This performs a full JavaScript Regex match while ignoring the case against the full path of the file on which the event has been triggered. It evaluates to true if it matches, false otherwise. This will trigger callbacks for all files that ends in `t` or `T`.
+//
+//       ["regex", "/t$/"]
+// * `name`: This evaluates to true if the name is exactly the same as the subexpression. For example, this will evaluate to true for test.txt.
+//
+//       ["name", "test.txt"]
+// * `iname`: This evaluates to true if the name is same as the subexpression when the case is ignored. For example, this will evaluate to true for test.txt or TEST.txt or TEst.tXt.
+//
+//       ["iname", "test.txt"]
+// * `empty`: This evaluates to true if the file is empty.
+//
+//       ["empty"]
+// * `exists`: This evaluates to true if the file exists.
+//
+//       ["exists"]
+// * `since`: This evaluates to true if the time property of the file is greater than the since (second in the expression) value. The time property needs to be specified as one of the filesystem metadata - `mtime`, `ctime`, `atime`.
+//
+//       ["since", 12345668, "mtime"]
+//       ["since", 12345668, "ctime"]
+//       ["since", 12345668, "atime"]
+// * `type`: Evaluates as true if the typ of file matches by the second argument. This matches regular files:
+//
+//       ["type", "f"]
+// Possible types are:
+//
+//   * `b`: block special file
+//   * `c`: character special file
+//   * `d`: directory
+//   * `f`: regular file
+//   * `p`: named pipe (FIFO)
+//   * `l`: symbolic link
+//   * `s`: socket
+//
+// ##Events
+//
+// There can be following event types for which the callbacks can be handled:
+//
+// * `add`: Fired when a file is added
+// * `change`: Fired when a watched file is changed
+// * `unlink`: Fired when a watched file is removed from disk
+// * `error`: Fired when an error is encountered
+//
+// These can be passed as a callback object when watching a file. The object is defined as follows:
+//
+//     { add: function(file) {...},
+//       change: function(file) {...},
+//       unlink: function(file) {...},
+//       error: function(file) {...}
+//     }
+// Then the function corresponding to the event will be called.
+//
+// ##Functions
+
 var fs = require("fs");
 var path = require("path");
 var chokidar = require("chokidar");
 
 var watchList = [];
 var watchers = [];
+
+
+// **watch**
+//
+// Function to start watching a file
+//
+// _Parameters:_
+//
+// * `file`: The file to watch
+// * `expression`: A valid expression in the JavaScript Array form
+// * `callback`: An object which can have the following named functions:
+//   * `add`: Called when a file is added
+//   * `change`: Called when a watched file is changed
+//   * `unlink`: Called when a watched file is removed from disk
+//   * `error`: Called when an error is encountered
+//
+// Any function in the callback object takes a an argument `file` which is the file that has been changed/created/deleted.
 
 var watch = function(file, expression, callback) {
     var obj = { file: file,
@@ -21,13 +146,41 @@ var watch = function(file, expression, callback) {
         watcher.on('error', wrapCallback(obj.callback.error, obj.expression));
     watchers.push(watcher);
 };
+exports.watch = watch;
 
-var watchList = function(file, expression, callback) {
+// **watchFiles**
+//
+// Function to start watching an array of files
+//
+// _Parameters:_
+//
+// * `file`: The list of files to watch
+// * `expression`: A list of valid expressions
+// * `callback`: A list of `callback` objects as decribed above
+//
+// _Throws:_
+//
+// * `Error`: If the length of all the three supplied lists are not same
+
+var watchFiles = function(file, expression, callback) {
     if(file.length != expression.length && expression.length != callback.length)
         throw new Error("All arguments must be of the same length");
     for(var i = 0; i < file.length; i++)
         watch(file[i], expression[i], callback[i]);
 };
+exports.watchList = watchList;
+
+// **deleteWatch**
+//
+// Function to stop watching a file
+//
+// _Parameters:_
+//
+// * `file`: The file to stop watching
+//
+// _Throws:_
+//
+// * `Error`: If the file is not being watched already
 
 var deleteWatch = function(file) {
     for(var i = 0; i < watchList.length; i++) {
@@ -40,22 +193,73 @@ var deleteWatch = function(file) {
     }
     throw new Error(file + " was not being watched previously");
 };
+exports.deleteWatch = deleteWatch;
+
+// **changeWatch**
+//
+// Function to change the parameters of a watcher
+//
+// _Parameters:_
+//
+// * `file`: The file whose watcher's parameters will be modified
+// * `expression`: The new expression against which the watcher will be matched
+// * `callback`: A valid callback object as described above.
+//
+// _Throws:_
+//
+// * `Error`: If the file is not being watched already
 
 var changeWatch = function(file, expression, callback) {
     deleteWatch(file);
     watch(file, expression, callback);
 };
+exports.changeWatch = changeWatch;
+
+// **changeTrigger**
+//
+// Function change the triggered callback on a watch event
+//
+// _Parameters:_
+//
+// * `file`: The file whose trigger is to be changed
+// * `event`: The event on which the trigger is to be executed. It can be one of:
+//   * `add`: Called when a file is added
+//   * `change`: Called when a watched file is changed
+//   * `unlink`: Called when a watched file is removed from disk
+//   * `error`: Called when an error is encountered
+// * `callback`: The function to be executed when the above event happens
+//
+// _Throws:_
+//
+// * `Error`: If the file is not being watched already
 
 var changeTrigger = function(file, event, callback) {
     for(var i = 0; i < watchList.length; i++) {
         if(watchList[i].file === file) {
-            watchers[i].on(event, wrapCallback(callback, watchList[i].expression);
+            watchers[i].on(event, wrapCallback(callback, watchList[i].expression));
             watchList[i].callback[event] = callback;
             return;
         }
     }
     throw new Error(file + " was not being watched previously");
 };
+exports.changeTrigger = changeTrigger;
+
+// **getTriggerList**
+//
+// Get the list of acting triggers on a file watcher
+//
+// _Parameters:_
+//
+// * `file`: The file whose triggers are to be returned
+//
+// _Returns:_
+//
+// The callback object for that file, with the named functions as `add`, `change`, `unlink`, `error` that had been set on that watcher
+//
+// _Throws:_
+//
+// * `Error`: If the file is not being watched already
 
 var getTriggerList = function(file) {
     for(var i = 0; i < watchList.length; i++) {
@@ -64,6 +268,20 @@ var getTriggerList = function(file) {
     }
     throw new Error(file + " was not being watched previously");
 };
+exports.getTriggerList = getTriggerList;
+
+// **deleteTrigger**
+//
+// Remove the trigger for an event from a watcher
+//
+// _Parameters:_
+//
+// * `file`: The file whose trigger is to be removed
+// * `event`: The event whose trigger is to be removed
+//
+// _Throws:_
+//
+// * `Error`: If the file is not being watched already
 
 var deleteTrigger = function(file, event) {
     var fn = function(file) {};
@@ -76,6 +294,7 @@ var deleteTrigger = function(file, event) {
     }
     throw new Error(file + " was not being watched previously");
 };
+exports.deleteTrigger = deleteTrigger;
 
 var findByExpression = function(expression) {
     var result = [];
@@ -85,6 +304,7 @@ var findByExpression = function(expression) {
     }
     return result;
 };
+exports.findByExpression = findByExpression;
 
 var changeExpression = function(file, expression) {
     for(var i = 0; i < watchList.length; i++) {
@@ -103,6 +323,7 @@ var changeExpression = function(file, expression) {
     }
     throw new Error(file + " was not being watched previously");
 };
+exports.changeExpression = changeExpression;
 
 function wrapCallback(realCallback, expression) {
     var fn = function(file) {
@@ -159,10 +380,10 @@ function parseAllOf(file, expression) {
 }
 
 function parseAnyOf(file, expression) {
-    var result = false;
     for(var i = 1; i < expression.length; i++)
-        result = result || parseExpression(file, expression[i]);
-    return result;
+        if(parseExpression(file, expression[i]))
+            return true;
+    return false;
 }
 
 function parseNot(file, expression) {
@@ -170,7 +391,7 @@ function parseNot(file, expression) {
 }
 
 function parseSuffix(file, expression) {
-    if(file.slice( -expression[1].length) === expression[1])
+    if(path.extname(file).toLowerCase() === expression[1].toLowerCase())
         return true;
     return false;
 }
@@ -240,10 +461,3 @@ function parseType(file, expression) {
             return stat.isSocket();
     }
 }
-
-//////////////////////////////////////////////////////////////////////////
-function demoCallback(file) {
-    console.log(file);
-}
-console.log("trying to test");
-watch("/media/Workspace/varchas-site.txt", ["allof", ["type", "f"], ["empty"]], {change: demoCallback});
